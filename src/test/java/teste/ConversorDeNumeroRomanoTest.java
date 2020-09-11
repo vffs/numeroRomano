a449170cdfbebc8fe515f4ea78594cd409850822
vffs <vffs@a.recife.ifpe.edu.br>
2020-09-11 14:14:01 -0300

    metodo deveEntenderOSimboloII

--- src/test/java/teste/ConversorDeNumeroRomanoTest.java
+++ src/test/java/teste/ConversorDeNumeroRomanoTest.java
@@ -27,4 +27,11 @@ public class ConversorDeNumeroRomanoTest {
         int numero = romano.converte("V");
         assertEquals(5, numero);
     }
+    
+    @Test 
+    public void deveEntenderOSimboloII(){
+        ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+        int numero = romano.converte("II");
+        assertEquals(2, numero);
+    }
 }
