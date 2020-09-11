77d2b790da21118566a56a7dac33f4b012506fe7
vffs <vffs@a.recife.ifpe.edu.br>
2020-09-11 14:28:49 -0300

    refatoramento do conversor

--- src/main/java/numeroRomanos/ConversorDeNumeroRomano.java
+++ src/main/java/numeroRomanos/ConversorDeNumeroRomano.java
@@ -10,21 +10,25 @@ import java.util.Map;
  */
 
 public class ConversorDeNumeroRomano {
-    private static  Map<String, Integer> tabela = new HashMap<String,Integer>(){
+    private static  Map<Character, Integer> tabela = new HashMap<Character,Integer>(){
         {
-            put("I",1);
-            put("V",5);
-            put("X",10);
-            put("L",50);
-            put("C",100);
-            put("D",500);
-            put("M",1000);
+            put('I',1);
+            put('V',5);
+            put('X',10);
+            put('L',50);
+            put('C',100);
+            put('D',500);
+            put('M',1000);
         }
     };
 
 
     public int converte(String numeroERomano) {
-        return tabela.get(numeroERomano);
+        int acumulador = 0;
+        for(int i = 0; i<numeroERomano.length();i++){
+            acumulador += tabela.get(numeroERomano.charAt(i));
+        }
+        return acumulador;
         
     }
     
