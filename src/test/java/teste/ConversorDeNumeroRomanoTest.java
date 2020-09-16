ec3e86f35f01885159b9ad2e8111973877fdd7e0
vffs <vffs@a.recife.ifpe.edu.br>
2020-09-16 12:55:41 -0300

    retirado código repetido, criado um método inicializa @Before

--- src/test/java/teste/ConversorDeNumeroRomanoTest.java
+++ src/test/java/teste/ConversorDeNumeroRomanoTest.java
@@ -2,6 +2,7 @@ package teste;
 
 import numeroRomanos.ConversorDeNumeroRomano;
 import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -10,38 +11,39 @@ import org.junit.Test;
  */
 
 public class ConversorDeNumeroRomanoTest {
+    ConversorDeNumeroRomano romano;
+    
+    @Before
+    public void inicializa(){
+        romano = new ConversorDeNumeroRomano();
+    }
     
     @Test
-    public void deveEntenderOSimboloI(){
-        ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+    public void deveEntenderOSimboloI(){       
         int numero = romano.converte("I");
         assertEquals(1, numero);
     }
     
     @Test
     public void deveEntenderOSimboloV(){
-        ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
         int numero = romano.converte("V");
         assertEquals(5, numero);
     }
     
     @Test 
     public void deveEntenderDoisSimbolosComoII(){
-        ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
         int numero = romano.converte("II");
         assertEquals(2, numero);
     }
     
     @Test
     public void deveEntenderQuatroSimboloDoisADoisComoXXII(){
-        ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
         int numero = romano.converte("XXII");
         assertEquals(22, numero);
     }
     
     @Test
     public void deveEntenderNumerosComoIX(){
-        ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
         int numero = romano.converte("IX");
         assertEquals(9, numero);
     }
